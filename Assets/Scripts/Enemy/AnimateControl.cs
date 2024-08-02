9699a931513bf0816a0f2ff9726e0591c67694c6
Thiago José da Silva Santos <42985926+ThiagoJose007@users.noreply.github.com>
2024-08-02 10:42:14 -0300

    :sparkels: feat: Criação de personagem e dinanimca de ataque

--- Assets/Scripts/Enemy/AnimateControl.cs
+++ Assets/Scripts/Enemy/AnimateControl.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AnimateControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void PlayAnim(int value)
     {
-        
+        anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+
+        }
+        else
+        {
+
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+    private Player player;
+    [SerializeField] private AnimateControl animControl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<Player>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
+
+        if(Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
+        {
+            animControl.PlayAnim(2);
+        }
+        else
+        {
+            animControl.PlayAnim(1);
+        }
+
+        float posX= player.transform.position.x - transform.position.x;
+        if(posX > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
