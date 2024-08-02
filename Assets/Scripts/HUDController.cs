8712140dad0d4aa5c186c51004085aa985535e83
Davi Souza <dsl@ic.ufal.br>
2024-08-02 10:39:24 -0300

    :sparkles: feat: items hud working

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+
+{
+    public PlayerItems playerItems;
+    public Text riceCount;
+    public Text slimeCount;
+    public Text rissotoCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCount();
+    }
+
+    void UpdateCount()
+    {
+        riceCount.text = playerItems.getTotalRice().ToString();
+        slimeCount.text = playerItems.getTotalSlime().ToString();
+        rissotoCount.text = playerItems.getTotalRisottoSlime().ToString();
+    }
+}
--- Assets/Scripts/PlayerItems.cs
+++ Assets/Scripts/PlayerItems.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerItems : MonoBehaviour
 {
-    public int totalRise;
+    public int totalRise = 2;
+    public int totalSlime = 1 ;
+    public int totalRisottoSlime = 1;
 
 
     // Start is called before the first frame update
@@ -17,5 +19,20 @@ public class PlayerItems : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public int getTotalRice()
+    {
+        return totalRise;
+    }
+
+    public int getTotalSlime()
+    {
+        return totalSlime;
+    }
+
+    public int getTotalRisottoSlime()
+    {
+        return totalRisottoSlime;
     }
 }
